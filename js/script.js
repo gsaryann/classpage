c476c34054215626d0c327ee21033133f8d82b71
gsaryann <50165574+gsaryann@users.noreply.github.com>
2021-10-09 14:44:10 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$('#owl-carousel').owlCarousel({
+    loop: true,
+    autoplay: false,
+    autoplayHoverPause: true,
+    slideTransition: 'linear',
+    items: 1,
+    nav: false,
+    dots: true,
+});
