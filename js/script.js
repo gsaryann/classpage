242c16f4d46cfc57634de4eb97a66e379eda1262
gsaryann <50165574+gsaryann@users.noreply.github.com>
2021-10-17 21:09:08 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -14,3 +14,43 @@ $("#menu").click(function() {
         $(this).toggleClass('active')
     })
 })
+
+
+function scheduleForDay() {
+    let nowDate = new Date();
+    let d = (nowDate.toLocaleString('ru', {
+        weekday: 'short'
+    }));
+
+    if (d == 'пн') {
+        let monday = document.getElementById('monday');
+        monday.style.display = 'flex';
+    }
+    if (d == 'вт') {
+        let tuesday = document.getElementById('tuesday');
+        tuesday.style.display = 'flex';
+    }
+    if (d == 'ср') {
+        let tuesday = document.getElementById('wednesday');
+        wednesday.style.display = 'flex';
+    }
+    if (d == 'чт') {
+        let thursday = document.getElementById('thursday');
+        thursday.style.display = 'flex';
+    }
+    if (d == 'пт') {
+        let friday = document.getElementById('friday');
+        friday.style.display = 'flex';
+    }
+    if (d == 'сб') {
+        let saturday = document.getElementById('saturday');
+        saturday.style.display = 'flex';
+    }
+    if (d == 'вс') {
+        let info = document.querySelector(".table__info")
+        info.style.display = 'flex';
+    }
+}
+window.onload = function() {
+    scheduleForDay();
+}
