ad06eff10fe47a59730508ffebc3d57765ca43cb
gsaryann <50165574+gsaryann@users.noreply.github.com>
2021-10-25 20:01:58 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ $('#owl-carousel').owlCarousel({
     items: 1,
     nav: false,
     dots: true,
+    center: true,
 });
 
 $("#menu").click(function() {
@@ -17,41 +18,84 @@ $("#menu").click(function() {
 
 
 function scheduleForDay() {
+    let secTable = document.querySelector('.table');
+    const schedule = {
+        'пн': {
+            'Время': 'Понедельник',
+            '8.00 - 8.40': 'Литература',
+            '8.40 - 9.20': 'География',
+            '9.40 - 10.20': 'Экономика',
+            '10.40 - 11.20': 'Экономика',
+            '11.40 - 12.20': 'Информатика',
+            '12.40 - 13.20': 'Родной русский язык',
+        },
+        'вт': {
+            'Время': 'Вторник',
+            '8.00 - 8.40': 'Литература',
+            '8.40 - 9.20': 'География',
+            '9.40 - 10.20': 'Экономика',
+            '10.40 - 11.20': 'Экономика',
+            '11.40 - 12.20': 'Информатика',
+            '12.40 - 13.20': 'Родной русский язык',
+        },
+        'ср': {
+            'Время': 'Среда',
+            '9.40 - 10.20': 'География',
+            '10.40 - 11.20': 'Английский язык',
+            '11.40 - 12.20': 'История',
+            '12.40 - 13.20': 'Математика',
+            '13.30 - 14.10': 'Математика',
+        },
+        'чт': {
+            'Время': 'Четверг',
+            '8.00 - 8.40': 'Физическая культура',
+            '8.50 - 9.30': 'Астрономия',
+            '9.40 - 10.20': 'География',
+            '10.40 - 11.20': 'Русский язык',
+            '11.40 - 12.20': 'Математика',
+            '12.40 - 13.20': 'Математика',
+            '13.30 - 14.10': 'Итоговое сочинение',
+        },
+        'пт': {
+            'Время': 'Пятница',
+            '8.00 - 8.40': 'ОБЖ',
+            '8.50 - 9.30': 'Математика',
+            '9.40 - 10.20': 'Математика',
+            '10.40 - 11.20': 'История',
+            '11.40 - 12.20': 'Право',
+            '12.40 - 13.20': 'Литература',
+            '13.30 - 14.10': 'Математика',
+        },
+        'сб': {
+            'Время': 'Суббота',
+            '8.00 - 8.40': 'Индивидуальный проект',
+            '8.50 - 9.30': 'Индивидуальный проект',
+            '9.40 - 10.20': 'Английский язык(дист.)',
+            '10.30 - 11.10': 'Физическая культура(дист.)',
+        },
+        'вс': [],
+    }
+
     let nowDate = new Date();
     let d = (nowDate.toLocaleString('ru', {
         weekday: 'short'
     }));
+    let table = document.createElement('table');
+    secTable.appendChild(table);
+    table.classList.toggle('table__content')
 
-    if (d == 'пн') {
-        let monday = document.getElementById('monday');
-        monday.style.display = 'flex';
-    }
-    if (d == 'вт') {
-        let tuesday = document.getElementById('tuesday');
-        tuesday.style.display = 'flex';
-    }
-    if (d == 'ср') {
-        let tuesday = document.getElementById('wednesday');
-        wednesday.style.display = 'flex';
-    }
-    if (d == 'чт') {
-        let thursday = document.getElementById('thursday');
-        thursday.style.display = 'flex';
-    }
-    if (d == 'пт') {
-        let friday = document.getElementById('friday');
-        friday.style.display = 'flex';
-    }
-    if (d == 'сб') {
-        let saturday = document.getElementById('saturday');
-        saturday.style.display = 'flex';
-    }
-    if (d == 'вс') {
-        let info = document.querySelector(".table__info")
-        info.style.display = 'flex';
+    for (let key in schedule[d]) {
+        let tr = document.createElement('tr');
+        table.appendChild(tr);
+        tr.classList.toggle('table__item')
+        let td = document.createElement('td');
+        let td2 = document.createElement('td');
+        tr.append(td)
+        tr.append(td2)
+        td.innerHTML = key;
+        td2.innerHTML = schedule[d][key]
     }
 }
 window.onload = function() {
-    console.log(1)
     scheduleForDay();
 }
